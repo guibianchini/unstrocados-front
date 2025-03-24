3b8c919bc3f879b1332634658ac7e440bff9a9cc
Guilherme <bianchiniguilherme0@gmail.com>
2025-03-24 10:48:49 -0300

    chore: configura Bootstrap, fonte Poppins e cores personalizadas

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+import './index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fontsource/poppins/400.css'; // Importa o peso normal da fonte
+import '@fontsource/poppins/600.css'; // Importa o peso bold da fonte
+
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
